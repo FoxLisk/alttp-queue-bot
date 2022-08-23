6c7853413a63686ed8c765e530397e784f6df6a5
Alexander Corwin <ancorwin@gmail.com>
2022-08-23 08:21:14 +0000

    This is not a webserver

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
 NGINX_TARGET_PATH="/etc/nginx/conf.d/$SERVICE_NAME.conf"
 cargo build
 sudo cp "conf_files/lib/systemd/system/$SERVICE_NAME.service" $SERVICE_PATH
-if [ -f $NGINX_TARGET_PATH ]; then
-  echo "$NGINX_TARGET_PATH already exists; not overwriting"
-else
-  sudo cp "conf_files/etc/nginx/conf.d/$SERVICE_NAME.conf" $NGINX_TARGET_PATH
-fi
 sudo systemctl daemon-reload
 sudo systemctl restart "$SERVICE_NAME"
 sudo systemctl enable "$SERVICE_NAME"
